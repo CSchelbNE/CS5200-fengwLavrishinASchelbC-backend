73b4093ea6c7ca98f0075498dd8388609fd96b1c
mac <mac@macs-Air.cable.rcn.com>
2022-12-07 05:52:53 -0500

    AWS RDS ONLINE TESTING CONN

--- database.py
+++ database.py
@@ -12,12 +12,12 @@ password = "F1shg0d12345!"
 LOCAL_HOST_URL = "mysql+pymysql://" + username + ":" + \
                  password + "@localhost"
 
-REMOTE_DATABASE_URL = "mysql+pymysql://admin:cs5200finalproject@database-1.c5mdh4lrufto.us-east-2.rds.amazonaws.com" \
+REMOTE_DATABASE_URL = "mysql+pymysql://admin:cs5200final@database-1.c5mdh4lrufto.us-east-2.rds.amazonaws.com" \
                       "/ticket_system"
 
 # aws remote server password is cs5200finalproject
-engine = sql.create_engine(LOCAL_HOST_URL,
-                           connect_args=dict(host='localhost', port=3306))
+engine = sql.create_engine(REMOTE_DATABASE_URL,
+                           connect_args=dict(host='database-1.c5mdh4lrufto.us-east-2.rds.amazonaws.com', port=3306))
 while True:
     try:
         conn = engine.connect()
@@ -29,7 +29,6 @@ while True:
     except Exception as error:
         print("Connection failed")
         print("Error:" + error.__str__())
-        break;
 
 
 def get_db():
