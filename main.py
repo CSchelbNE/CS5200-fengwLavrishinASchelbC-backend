062abfa538d302ab9ee3205877911fa9ebd0299d
mac <mac@macs-Air.cable.rcn.com>
2022-12-07 06:19:36 -0500

    cors take 4

--- main.py
+++ main.py
@@ -28,16 +28,16 @@ app.include_router(technician_router)
 # app.include_router(auth.router)
 #
 # Handles cross origin requests from front and backend
-origins = [
-    "http://localhost:3000",
-    "https://localhost:3000/",
-    "localhost:3000/*",
-    "https://storied-cheesecake-d437f5.netlify.app:0/*"
-]
+# origins = [
+#     "http://localhost:3000",
+#     "https://localhost:3000/",
+#     "localhost:3000/*",
+#     "https://storied-cheesecake-d437f5.netlify.app:0/*"
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
