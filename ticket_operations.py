3151ceeccb6d27d5134c3edab47545643851b229
mac <mac@macs-Air.cable.rcn.com>
2022-12-08 14:37:56 -0500

    new err

--- ticket_operations.py
+++ ticket_operations.py
@@ -32,6 +32,9 @@ def get_users_tickets(user_id: int, db: Engine = Depends(get_db)):
         except sqlalchemy.exc.InternalError as err:
             trans.rollback()
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INVALID REQ")
+        except sqlalchemy.exc.InterfaceError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INTERFACE ERROR")
 
 
 # SELECT * FROM ticket NATURAL JOIN problem WHERE user_id = %s
@@ -86,10 +89,23 @@ def edit_ticket(ticket: Ticket, ticket_id: int, db: Engine = Depends(get_db)):
 @ticket_router.delete("/delete-ticket/{ticket_id}")
 def delete_ticket(ticket_id: int, db: Engine = Depends(get_db)):
     conn = db.connect()
-    trans = conn.begin()
-    conn.execute(f"""DELETE FROM ticket WHERE ticket_id = %s""", str(ticket_id))
-    trans.commit()
-    return {"ticket_id": ticket_id}
+    with conn.begin() as trans:
+        try:
+            conn.execute(f"""DELETE FROM ticket WHERE ticket_id = %s""", str(ticket_id))
+            trans.commit()
+            return {"ticket_id": ticket_id}
+        except sqlalchemy.exc.PendingRollbackError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="ROLLBACK OCCURRED")
+        except sqlalchemy.exc.OperationalError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="OPS ERROR")
+        except sqlalchemy.exc.InvalidRequestError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INVALID REQ")
+        except sqlalchemy.exc.InternalError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INVALID REQ")
 
 
 @ticket_router.post("/create-ticket")  # something here for if ticket == hardware: trigger
@@ -146,6 +162,10 @@ def get_comments(ticket_id: int, db: Engine = Depends(get_db)):
         except sqlalchemy.exc.InternalError as err:
             trans.rollback()
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INTERNAL ERROR")
+        except sqlalchemy.exc.InterfaceError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INTERFACE ERROR")
+
 
 
 @ticket_router.post("/complete-survey/{ticket_id}")
@@ -163,7 +183,6 @@ def complete_survey(ticket_id: int, survey: Survey, db: Engine = Depends(get_db)
             trans.rollback()
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="OPS ERROR")
         except sqlalchemy.exc.InvalidRequestError as err:
-            print("HERE")
             trans.rollback()
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INVALID REQ")
         except sqlalchemy.exc.InternalError as err:
