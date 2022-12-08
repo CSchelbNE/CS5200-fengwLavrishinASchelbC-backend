ec3fbbbbfcb04ec04651ad5b908871ae67d0281c
mac <mac@macs-Air.cable.rcn.com>
2022-12-08 14:32:51 -0500

    all error handling added

--- admin_operations.py
+++ admin_operations.py
@@ -1,6 +1,7 @@
+import sqlalchemy.exc
 from database import get_db
 from sqlalchemy.engine import Engine
-from fastapi import Depends
+from fastapi import status, HTTPException, Depends
 from fastapi import APIRouter
 from schemas import Approval
 
@@ -12,13 +13,43 @@ admin_router = APIRouter(
 
 @admin_router.get("/get-all-approvals")
 def get_approvals(db: Engine = Depends(get_db)):
-    return db.execute("""CALL getApprovals()""").all()
+    conn = db.connect()
+    with conn.begin() as trans:
+        try:
+            result =  db.execute("""CALL getApprovals()""").all()
+            trans.commit()
+            return result
+        except sqlalchemy.exc.PendingRollbackError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="ROLLBACK OCCURRED")
+        except sqlalchemy.exc.OperationalError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="OPS ERROR")
+        except sqlalchemy.exc.InvalidRequestError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INVALID REQ")
+        except sqlalchemy.exc.InternalError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INTERNAL ERROR")
 
 
 @admin_router.put("/status-change/{approval_id}")
 def change_status(approval: Approval, approval_id: int, db: Engine = Depends(get_db)):
     conn = db.connect()
-    trans = conn.begin()
-    edited_approval = db.execute(f"""CALL editApprovalStatus(%s, %s)""", (int(approval_id), str(approval.status))).first()
-    trans.commit()
-    return edited_approval
+    with conn.begin() as trans:
+        try:
+            edited_approval = db.execute(f"""CALL editApprovalStatus(%s, %s)""", (int(approval_id), str(approval.status))).first()
+            trans.commit()
+            return edited_approval
+        except sqlalchemy.exc.PendingRollbackError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="ROLLBACK OCCURRED")
+        except sqlalchemy.exc.OperationalError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="OPS ERROR")
+        except sqlalchemy.exc.InvalidRequestError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INVALID REQ")
+        except sqlalchemy.exc.InternalError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INTERNAL ERROR")
--- end_user_operations.py
+++ end_user_operations.py
@@ -18,14 +18,28 @@ end_user_router = APIRouter(
 def add_new_user(user: User, db: Engine = Depends(get_db)):
     hashed_password = hash(user.password)  # hashed pw is stored in models.User.password
     conn = db.connect()
-    trans = conn.begin()
-    new_user = conn.execute(f"""CALL createUser(%s,%s,%s,%s,%s,%s)""", (str(hashed_password),
+    with conn.begin() as trans:
+        try:
+            new_user = conn.execute(f"""CALL createUser(%s,%s,%s,%s,%s,%s)""", (str(hashed_password),
                                                                         str(user.type), str(user.name),
                                                                         str(user.address),
                                                                         str(user.email), str(user.campus))).first()
+            trans.commit()
+            return new_user
+        except sqlalchemy.exc.PendingRollbackError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="ROLLBACK OCCURRED")
+        except sqlalchemy.exc.OperationalError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="OPS ERROR")
+        except sqlalchemy.exc.InvalidRequestError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INVALID REQ")
+        except sqlalchemy.exc.InternalError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INTERNAL ERROR")
 
-    trans.commit()
-    return new_user
+
 
 
 @end_user_router.get("/user", response_model_exclude_none=True)
@@ -35,25 +49,34 @@ def get_users(db: Engine = Depends(get_db)):
 
 @end_user_router.post("/login", response_model_exclude_none=True)
 def login(credentials: Credentials, db: Engine = Depends(get_db)):
-    print(credentials)
-    try:
-        conn = db.connect()
-        trans = conn.begin()
-        # in DB - find 1st matching username
-        result = conn.execute(f"""SELECT * FROM users WHERE name = %s""", (str(credentials.username),)).first()
-        trans.commit()
-        if verify_password(credentials.password, result.password):
-            return result  # if inputted pw matches stored(hashed) pw, return the user
-    except sqlalchemy.exc.OperationalError:
-        trans.rollback()
-        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Internal server outage")
+    conn = db.connect()
+    with conn.begin() as trans:
+        try:
+            conn = db.connect()
+            trans = conn.begin()
+            # in DB - find 1st matching username
+            result = conn.execute(f"""SELECT * FROM users WHERE name = %s""", (str(credentials.username),)).first()
+            trans.commit()
+            if verify_password(credentials.password, result.password):
+                return result  # if inputted pw matches stored(hashed) pw, return the user
 
+        except sqlalchemy.exc.PendingRollbackError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="ROLLBACK OCCURRED")
+        except sqlalchemy.exc.OperationalError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="OPS ERROR")
+        except sqlalchemy.exc.InvalidRequestError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INVALID REQ")
+        except sqlalchemy.exc.InternalError as err:
+            trans.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="INTERNAL ERROR")
 
+        except AttributeError:
+            print("***** AttributeError: Ensure username is valid *****")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"Invalid login credentials")
 
-    except AttributeError:
-        print("***** AttributeError: Ensure username is valid *****")
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Invalid login credentials")
-
-    except passlib.exc.UnknownHashError:
-        print("***** passlib.exc.UnknownHashError: User potentially has unhashed password stored in DB *****")
+        except passlib.exc.UnknownHashError:
+            print("***** passlib.exc.UnknownHashError: User potentially has unhashed password stored in DB *****")
